437fca54f11d4565e50038397de3874df280b356
Subhajit Sahu <wolfram77@gmail.com>
2021-04-11 03:12:30 +0530

    :microbe: w/ readDescMan()

--- build.js
+++ build.js
@@ -4,8 +4,10 @@ const cp = require('child_process');
 const path = require('path');
 
 const SUPER_URL = 'https://github.com/tj/git-extras';
-const RDESC = /^\s+\"([\w-]+):\s*(.*?)\"(?:\s*\\)$/gm;
-const RBANG = /^(#!\S+\s+\S+)/;
+const RDESCETC = /^\s+\"([\w-]+):\s*(.*?)\"(?:\s*\\)$/gm;
+const RDESCMAN = /^\s+-\s+\*\*([\w-]+)\(\d\)\*\*\s(.*)$/gm;
+const RNOTTEXT = /[^\w\s\'\(\)-\/]/g;
+const RSHEBANG = /^(#!\S+\s+\S+)\n*/;
 
 
 
@@ -29,29 +31,50 @@ function fetchSuper() {
 }
 
 
-function readDesc(dir) {
+function cleanSuper(dir) {
+  cp.execSync(`rm -rf "${dir}"`);
+}
+
+
+function readDescEtc(dir) {
   var f = path.join(dir, 'etc', 'git-extras.fish');
   var d = readFile(f), m, a = new Map();
-  while ((m=RDESC.exec(d)) != null)
+  while ((m=RDESCETC.exec(d)) != null)
     a.set(m[1], m[2]);
   return a;
 }
 
 
+function readDescMan(dir) {
+  var f = path.join(dir, 'man', 'git-extras.md');
+  var d = readFile(f), m, a = new Map();
+  while ((m=RDESCMAN.exec(d)) != null)
+    a.set(m[1], m[2].replace(RNOTTEXT, ''));
+  return a;
+}
+
+
+function readDesc(dir) {
+  return readDescEtc(dir);
+}
+
+
 function copyBin(dir, desc) {
+  fs.mkdirSync('bin', {recursive: true});
   var bin = path.join(dir, 'bin');
   for (var f of fs.readdirSync(bin)) {
     if (f === 'git-extras') continue;
     var g = f.replace('git-', '');
     var d = readFile(`${bin}/${f}`);
     var c = desc.get(g)||'';
-    d = d.replace(RBANG, `$1\n\n## ${c}`)
-    writeFile(`bin/${g}`, d);
+    d = d.replace(RSHEBANG, `$1\n## ${c}\n\n`)
+    writeFile(`bin/${g}.sh`, d);
   }
 }
 
 
 function copyMan(dir) {
+  fs.mkdirSync('man', {recursive: true});
   var man = path.join(dir, 'man');
   for (var f of fs.readdirSync(man)) {
     if (path.extname(f) !== '.md') continue;
@@ -68,5 +91,6 @@ function main() {
   var desc = readDesc(dir);
   copyBin(dir, desc);
   copyMan(dir);
+  cleanSuper(dir);
 }
 main();
