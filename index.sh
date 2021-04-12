1b2c0e7bdcf5e9710f8511025e7f02fcac8ace4b
Subhajit Sahu <wolfram77@gmail.com>
2021-04-13 00:43:53 +0530

    :microbe: update index.sh description

--- index.sh
+++ index.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-## GIT utilities -- repo summary, repl, changelog population, author commit percentages and more.
+## Common utility commands for Git repositories.
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cmd="$dir/bin/$1.sh"
