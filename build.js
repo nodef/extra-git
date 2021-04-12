80dcad3afa9ab5f7bb700bd34ccebfca06dfdbed
Subhajit Sahu <wolfram77@gmail.com>
2021-04-12 16:08:05 +0530

    :microbe: convert markdown to text

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const os = require('os');
 const cp = require('child_process');
 const path = require('path');
+const markdownToText = require('markdown-to-text').default;
 
 const SUPER_URL = 'https://github.com/tj/git-extras';
 const RDESCETC = /^\s+\"([\w-]+):\s*(.*?)\"(?:\s*\\)$/gm;
@@ -64,7 +65,7 @@ function copyBin(dir, desc) {
   var bin = path.join(dir, 'bin');
   for (var f of fs.readdirSync(bin)) {
     if (f === 'git-extras') continue;
-    var g = f.replace('git-', '');
+    var g = f.replace(/^git-/g, '');
     var d = readFile(`${bin}/${f}`);
     var c = desc.get(g)||'';
     d = d.replace(RSHEBANG, `$1\n## ${c}\n\n`)
@@ -79,9 +80,10 @@ function copyMan(dir) {
   for (var f of fs.readdirSync(man)) {
     if (path.extname(f) !== '.md') continue;
     if (f === 'git-extras.md') continue;
-    var g = f.replace('git-', '');
+    var g = f.replace(/^git-|\.md$/g, '');
     var d = readFile(`${man}/${f}`);
-    writeFile(`man/${g}`, d);
+    d = markdownToText(d);
+    writeFile(`man/${g}.txt`, d);
   }
 }
 
