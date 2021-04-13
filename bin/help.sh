2a1fe02ab6739806e531d2a43249580b3adc1204
Subhajit Sahu <wolfram77@gmail.com>
2021-04-13 15:30:49 +0530

    :microbe: use less for scroll help

--- bin/help.sh
+++ bin/help.sh
@@ -4,6 +4,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 url="https://github.com/nodef/extra-git/issues"
 
 fil="$dir/../man/$1.txt"
-if   [[ "$1" == "" ]]; then cat "$dir/../man/help.txt"
-elif [ -f "$fil" ];    then cat "$fil"
+if   [[ "$1" == "" ]]; then less "$dir/../man/help.txt"
+elif [ -f "$fil" ];    then less "$fil"
 else echo "No help found for '$1'. You can report this at ${url}"; fi
