476009048624c23b696b040ad3d5d52c8c7eefc5
Subhajit Sahu <wolfram77@gmail.com>
2021-04-13 15:04:54 +0530

    :microbe: report error if help if not found

--- bin/help.sh
+++ bin/help.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 ## Print usage details of a command.
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-fil="$dir/../man/$1.txt"
+url="https://github.com/nodef/extra-git/issues"
 
-if [ -f "$fil" ]; then cat "$fil"; else cat "$dir/../man/help.txt"; fi
+fil="$dir/../man/$1.txt"
+if   [[ "$1" == "" ]]; then cat "$dir/../man/help.txt"
+elif [ -f "$fil" ];    then cat "$fil"
+else echo "No help found for '$1'. You can report this at ${url}"
