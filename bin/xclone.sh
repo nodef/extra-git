6ad9002c5eda2f675971d5730051e0c9761d6012
Subhajit Sahu <wolfram77@gmail.com>
2021-04-13 15:12:24 +0530

    :microbe: fix xclone

--- bin/xclone.sh
+++ bin/xclone.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 ## Clone repository to local (inc. submodules).
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
 url="$( "$dir/url.sh" "$1" )"
-repo="$2"
-
-
-git clone "$url" "$2"
-if [[ "$repo" == "" ]]; then repo="$( "$dir/name.sh" "$1" )"
-cd "$repo" && "$dir/init-submodules.sh"
+if [[ "$2" == "" ]]; then repo="$( "$dir/name.sh" "$1" )"; else repo="$2"; fi
+git clone "$url" "$repo" && cd "$repo"
+"$dir/init-submodules.sh"
