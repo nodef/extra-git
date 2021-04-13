c02364351bce10cdcd03f88f565d355a146b3cfa
Subhajit Sahu <wolfram77@gmail.com>
2021-04-13 15:28:09 +0530

    :microbe: fix help

--- bin/help.sh
+++ bin/help.sh
@@ -6,4 +6,4 @@ url="https://github.com/nodef/extra-git/issues"
 fil="$dir/../man/$1.txt"
 if   [[ "$1" == "" ]]; then cat "$dir/../man/help.txt"
 elif [ -f "$fil" ];    then cat "$fil"
-else echo "No help found for '$1'. You can report this at ${url}"
+else echo "No help found for '$1'. You can report this at ${url}"; fi
--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cmd="$dir/bin/$1.sh"
-if   [[ "$2" == "--help" ]]; then "$dir/help.sh" "$1"
-elif [[ "$1" == "--help" ]]; then "$dir/help.sh"
+if   [[ "$2" == "--help" ]]; then "$dir/bin/help.sh" "$1"
+elif [[ "$1" == "--help" ]]; then "$dir/bin/help.sh"
 elif [ -f "$cmd" ]; then shift; $cmd "$@"
 else git "$@"; fi
