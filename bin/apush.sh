d705d29303a3597bc478711140a79f9a99d61610
Subhajit Sahu <wolfram77@gmail.com>
2021-04-14 09:02:10 +0530

    :microbe: fix apush

--- bin/apush.sh
+++ bin/apush.sh
@@ -2,5 +2,5 @@
 ## Amend current changes and push (inc. submodules).
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-"$dir/apush-submodule.sh" "$@"
+"$dir/apush-submodules.sh" "$@"
 git add . && git commit --amend --no-edit && git push -f
