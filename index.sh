335775b187e17619b6604815f69f03a2aaea566b
Subhajit Sahu <wolfram77@gmail.com>
2021-04-17 21:13:39 +0530

    fix command call, when path has spaces

--- index.sh
+++ index.sh
@@ -5,5 +5,5 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cmd="$dir/bin/$1.sh"
 if   [[ "$2" == "--help" ]]; then "$dir/bin/help.sh" "$1"
 elif [[ "$1" == "--help" ]]; then "$dir/bin/help.sh"
-elif [ -f "$cmd" ]; then shift; $cmd "$@"
+elif [ -f "$cmd" ]; then shift; "$cmd" "$@"
 else git "$@"; fi
